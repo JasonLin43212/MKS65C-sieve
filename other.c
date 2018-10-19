82ca692405f1682291b2e5ff8fce9f4773bda048
Jason Lin <jasonlin43212@gmail.com>
2018-10-19 18:00:05 -0400

    removed all even numbers

---
+++ other.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+/*
+//Verision 2 stopping at square root
+int sieve(double n) {
+  int array_size;
+  if (n>5000) {
+    array_size = (int)(1.15*n*log(n));
+  }
+  else {
+    array_size = (int)(1.3*n*log(n)+10);
+  }
+  char * nums = calloc(array_size,sizeof(char));
+
+  int current_n = 0;
+  int current_num = 1;
+  int stopping_point = (int)sqrt(array_size);
+  while (current_n != n) {
+    current_num++;
+    if (!(*(nums+current_num))){
+      if (current_num < stopping_point){
+        int temp_index = 2*current_num;
+        while (temp_index < array_size){
+          char * temp = nums+temp_index;
+          if (!(*temp)){
+            *temp = 1;
+          }
+          temp_index += current_num;
+        }
+      }
+      current_n++;
+    }
+  }
+  return current_num;
+}
+*/
+
+/* Version 1 naive approach
+
+   int sieve(double n) {
+   int array_size = 1;
+   if (n>5000) {
+   array_size = (int)(1.15*n*log(n));
+   }
+   else {
+   array_size = (int)(1.3*n*log(n)+10);
+   }
+   char * nums = calloc(array_size,sizeof(char));
+
+   int current_n = 0;
+   int current_num = 1;
+   while (current_n != n) {
+   current_num++;
+   if (!(*(nums+current_num))){
+   int temp_index = 2*current_num;
+   while (temp_index < array_size){
+   char * temp = nums+temp_index;
+   if (!(*temp)){
+   *temp = 1;        }
+   temp_index += current_num;
+   }
+   current_n++;
+   }
+   }
+   return current_num;
+   }
+*/
--- sieve.c
+++ sieve.c
@@ -2,73 +2,37 @@
 #include <stdlib.h>
 #include <math.h>
 
-void print_ints(char * c, int size) {
-  int current = 0;
-  while (current < size) {
-    printf("%d: %d\n",current+2,*c);
-    c++;
-    current++;
-  }
-}
-
-//Verision 2 removing all even nums
 int sieve(double n) {
   int array_size;
   if (n>5000) {
-    array_size = (int)(1.15*n*log(n));
+    array_size = (int)(0.575*n*log(n));
   }
   else {
-    array_size = (int)(1.3*n*log(n)+10);
+    array_size = (int)(0.65*n*log(n)+5);
   }
   char * nums = calloc(array_size,sizeof(char));
 
-  int current_n = 0;
-  int current_num = 1;
+  if (n == 1) {
+    return 2;
+  }
+  int current_n = 1;
+  int current_num = 0;
   int stopping_point = (int)sqrt(array_size);
   while (current_n != n) {
     current_num++;
-    if (!(*(nums+current_num))){
+    if (!*(nums+current_num)){
       if (current_num < stopping_point){
-        int temp_index = 2*current_num;
+        int temp_index = 3*current_num+1;
         while (temp_index < array_size){
           char * temp = nums+temp_index;
-          if (!(*temp)){
-            *temp = 1;        }
-          temp_index += current_num;
+          if (!*temp){
+            *temp = 1;
+          }
+          temp_index += 2*current_num + 1;
         }
       }
       current_n++;
     }
   }
-  return current_num;
+  return 2*current_num+1;
 }
-
-/* Version 1 naive approach
-   int sieve(double n) {
-   int array_size = 1;
-   if (n>5000) {
-   array_size = (int)(1.15*n*log(n));
-   }
-   else {
-   array_size = (int)(1.3*n*log(n)+10);
-   }
-   char * nums = calloc(array_size,sizeof(char));
-
-   int current_n = 0;
-   int current_num = 1;
-   while (current_n != n) {
-   current_num++;
-   if (!(*(nums+current_num))){
-   int temp_index = 2*current_num;
-   while (temp_index < array_size){
-   char * temp = nums+temp_index;
-   if (!(*temp)){
-   *temp = 1;        }
-   temp_index += current_num;
-   }
-   current_n++;
-   }
-   }
-   return current_num;
-   }
-*/
