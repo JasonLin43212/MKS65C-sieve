dbee6e992603846ccdd91364c87e5885f440038a
Jason Lin <jasonlin43212@gmail.com>
2018-10-19 18:28:51 -0400

    tried to optimize

--- sieve.c
+++ sieve.c
@@ -11,28 +11,28 @@ int sieve(double n) {
     array_size = (int)(0.65*n*log(n)+5);
   }
   char * nums = calloc(array_size,sizeof(char));
-
-  if (n == 1) {
+  if (n != 1){
+    int current_n = 1;
+    int current_num = 0;
+    while (current_n != n) {
+      current_num++;
+      if (!*(nums+current_num)){
+        if (current_num * current_num < array_size){
+          int temp_index = current_num + current_num + current_num + 1;
+          while (temp_index < array_size){
+            char * temp = nums+temp_index;
+            if (!*temp){
+              *temp = 1;
+            }
+            temp_index += current_num + current_num + 1;
+          }
+        }
+        current_n++;
+      }
+    }
+    return current_num + current_num+1;
+  }
+  else {
     return 2;
   }
-  int current_n = 1;
-  int current_num = 0;
-  int stopping_point = (int)sqrt(array_size);
-  while (current_n != n) {
-    current_num++;
-    if (!*(nums+current_num)){
-      if (current_num < stopping_point){
-        int temp_index = 3*current_num+1;
-        while (temp_index < array_size){
-          char * temp = nums+temp_index;
-          if (!*temp){
-            *temp = 1;
-          }
-          temp_index += 2*current_num + 1;
-        }
-      }
-      current_n++;
-    }
-  }
-  return 2*current_num+1;
 }
